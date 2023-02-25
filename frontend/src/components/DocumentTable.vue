d11f4d9b03fa9fb0bef380eaafc91f2056176d12
ly747688491 <747688491@qq.com>
2023-02-25 10:31:50 +0800

    后端增加注册接口 前端新增最近编辑和回收站页面

---
+++ frontend/src/components/DocumentTable.vue
@@ -0,0 +1,44 @@
+<template>
+    <el-table
+      :data="tableData"
+      height="250"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="文件名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="最近编辑"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="创建时间"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          tableData: [{
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }]
+        }
+      }
+    }
+  </script>
---
+++ frontend/src/views/Editview.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="home">
+      <el-container>
+          <el-aside width="300px" style="overflow-x: hidden;">
+                  <SideMenu/>
+          </el-aside>
+          <el-container>
+              <el-header>
+                  <header-component></header-component>
+              </el-header>
+              <el-main>
+                  
+              </el-main>
+          </el-container>
+      </el-container>
+    </div>
+  </template>
+  
+  <script>
+  import SideMenu from '@/components/SideMenu.vue';
+  import HeaderComponent from '@/components/HeaderComponent.vue';
+  
+  
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      SideMenu,
+      HeaderComponent,
+  }
+  }
+  </script>
+  
+  <style scoped>
+  
+  .el-header{
+      background-color: #B3C0D1;
+      color: #333;
+      text-align: center;
+      line-height: 100px;
+      height: 100px;
+    }
+  
+    .el-aside {
+      height: 100vh;
+    }
+    
+    .el-main {
+      background-color: #E9EEF3;
+      color: #333;
+      text-align: center;
+    }
+  </style>
